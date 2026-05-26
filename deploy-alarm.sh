4b4b3d562ff290eb853a03f853f23594783a215d
acastrillon305 <acastrillon1290@gmail.com>
2026-05-26 12:41:15 -0400

    Create deploy-alarm.sh
    Add CloudWatch deployment script

---
+++ deploy-alarm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudwatch put-metric-alarm \
+  --alarm-name HighCPUUtilization \
+  --metric-name CPUUtilization \
+  --namespace AWS/EC2 \
+  --statistic Average \
+  --period 300 \
+  --threshold 80 \
+  --comparison-operator GreaterThanThreshold \
+  --evaluation-periods 2 \
+  --alarm-actions arn:aws:sns:us-east-1:123456789012:OpsAlerts
